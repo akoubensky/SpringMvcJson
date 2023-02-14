c8392a53d385195b5d63539e3bb9ed472e07b6dc
akoub <akoubensky@gmail.com>
2023-02-14 20:39:31 +0300

    Initial commit

---
+++ src/main/java/com/example/controllers/PaymentController.java
@@ -0,0 +1,29 @@
+package com.example.controllers;
+
+import com.example.model.PaymentDetails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class PaymentController {
+
+  private static Logger logger =
+      Logger.getLogger(PaymentController.class.getName());
+
+  @PostMapping("${payment.request}")
+  public ResponseEntity<PaymentDetails> makePayment(
+      @RequestBody PaymentDetails paymentDetails) {
+      logger.info("Received payment " + paymentDetails.getAmount());
+      return ResponseEntity
+              .status(HttpStatus.OK)
+              .contentType(MediaType.APPLICATION_JSON)
+              .body(paymentDetails);
+  }
+}
